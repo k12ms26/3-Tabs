5b8124237a162e9fa6e192b0d4fefe406bb89ab5
k12ms26 <meeeeensun@gmail.com>
2020-12-30 21:20:08 +0900

    to push

--- app/src/main/java/com/example/cs496_tabbed/fragments/FreeFragment.kt
+++ app/src/main/java/com/example/cs496_tabbed/fragments/FreeFragment.kt
@@ -24,3 +24,4 @@ class FreeFragment : Fragment() {
     }
 
 }
+
--- app/src/main/java/com/example/cs496_tabbed/fragments/GalleryFragment.kt
+++ app/src/main/java/com/example/cs496_tabbed/fragments/GalleryFragment.kt
@@ -1,23 +1,31 @@
 package com.example.cs496_tabbed.fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.cs496_tabbed.R
 
-class GalleryFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
+class GalleryFragment : Fragment(), View.OnClickListener {
+    var myButton: ImageView? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery, container, false)
+    ): View {
+        val myView: View = inflater.inflate(R.layout.fragment_gallery, container, false)
+        myButton = myView.findViewById<View>(R.id.image1) as ImageView
+        myButton!!.setOnClickListener(this)
+        return myView
+    }
+    override fun onClick(v: View) {
+        if(v.id == R.id.image1) {
+            //클릭했을때 띄울 수 있는지
+            Log.d("TEST", "TEST")
+        }
     }
 }
