a85a9416c9b98912dab821b4f4d3be26c6a22077
hyrumk <hyrumk@kaist.ac.kr>
2021-01-03 21:11:24 +0900

    Add Color to Tic Tac Toe

--- app/src/main/java/com/example/cs496_tabbed/game/GameActivity.kt
+++ app/src/main/java/com/example/cs496_tabbed/game/GameActivity.kt
@@ -1,5 +1,7 @@
 package com.example.cs496_tabbed.game
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.icu.text.Transliterator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -25,8 +27,33 @@ class GameActivity : AppCompatActivity() {
     private lateinit var player1Points: TextView
     private lateinit var player2Points: TextView
 
+    //COLOR SETTING TOP//
+    lateinit var sharedPreferences: SharedPreferences
+    val themeKey = "currentTheme";val langKey = "currentLang"
+    //COLOR SETTING BOTTOM//
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //COLOR SETTING TOP//
+        sharedPreferences = getSharedPreferences(
+            "ThemePref",
+            Context.MODE_PRIVATE
+        )
+
+        when (sharedPreferences.getString(themeKey, "Color0")) {
+            "Color0" ->  theme.applyStyle(R.style.Color0, true)
+            "Color1" ->  theme.applyStyle(R.style.Color1, true)
+            "Color2" ->  theme.applyStyle(R.style.Color2, true)
+            "Color3" ->  theme.applyStyle(R.style.Color3, true)
+            "Color4" ->  theme.applyStyle(R.style.Color4, true)
+            "Color5" ->  theme.applyStyle(R.style.Color5, true)
+            "Color6" ->  theme.applyStyle(R.style.Color6, true)
+            "Color7" ->  theme.applyStyle(R.style.Color7, true)
+            "Color8" ->  theme.applyStyle(R.style.Color8, true)
+        }
+        //COLOR SETTING BOTTOM//
+
         setContentView(R.layout.activity_game)
 
         gameManager = GameManager()
