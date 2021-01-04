4bd754efeaea0945b5302377fec5081672d9f307
k12ms26 <meeeeensun@gmail.com>
2021-01-04 13:31:43 +0900

    틱택토 버튼 언어기능 추가

--- app/src/main/java/com/example/cs496_tabbed/game/GameMainActivity.kt
+++ app/src/main/java/com/example/cs496_tabbed/game/GameMainActivity.kt
@@ -6,13 +6,23 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.example.cs496_tabbed.MainActivity
 import com.example.cs496_tabbed.R
+import kotlinx.android.synthetic.main.activity_game_main.*
 
 class GameMainActivity : AppCompatActivity() {
     lateinit var startNewGameButton: Button
+    lateinit var titleTextView: TextView
     lateinit var sharedPreferences: SharedPreferences
     val themeKey = "currentTheme"; val langKey = "currentLang"
+    var listOfSet = mutableListOf<String>()
+    val listOfSet_eng = mutableListOf("Edit Date", "Edit Time", "Browser", "Tic Tac Toe", "Finish App")
+    val listOfSet_kor = mutableListOf("날짜 수정", "시간 수정", "브라우저", "틱택토", "앱 종료")
+    val listOfSet_chi = mutableListOf("日期修正","时间修正","浏览器", "井字游戏", "结束") // NEED MODIFICATION
+    val listOfSet_fra = mutableListOf("Modification la Date", "Modification du Temps","logiciel de navigation","Jeu de Morpion", "Fin") // NEED MODIFICATION
+    val listOfSet_esp = mutableListOf("Modificación de Fecha", "Modificación de Tiempo","Navegador","Tres en Línea", "Conclusión") // NEED MODIFICATION
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,16 +42,18 @@ class GameMainActivity : AppCompatActivity() {
             "Color7" -> theme.applyStyle(R.style.Color7, true)
             "Color8" -> theme.applyStyle(R.style.Color8, true)
         }
-        /*when(sharedPreferences.getString(langKey, "eng")){
-            "eng" -> listOfGameBtn = listOfGameBtn_eng
-            "kor" -> listOfGameBtn = listOfGameBtn_kor
-            "chi" -> listOfGameBtn = listOfGameBtn_chi
-            "fra" -> listOfGameBtn = listOfGameBtn_fra
-            "esp" -> listOfGameBtn = listOfGameBtn_esp
-        }*/
+
         setContentView(R.layout.activity_game_main)
 
         startNewGameButton = findViewById(R.id.startNewGameButton)
+        //titleTextView = findViewById(R.id.titleTextView)
+        when(sharedPreferences.getString(langKey, "eng")){
+            "eng" -> startNewGameButton.text = "Start New Game"
+            "kor" -> startNewGameButton.text = "게임 시작하기"
+            "chi" -> startNewGameButton.text = "游戏开始"
+            "fra" -> startNewGameButton.text = "Début du jeu"
+            "esp" -> startNewGameButton.text = "¡A jugar!"
+        }
 
         startNewGameButton.setOnClickListener {
             val intent = Intent(GameMainActivity@this, GameActivity::class.java)
